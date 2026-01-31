23440b00a449b93e871c6b314009d1ddbddaca35
DmitriyArcher1 <d.kartrajt@gmail.com>
2026-01-31 13:44:13 +0300

    Fix error middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,14 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
 
-// Приватный роутер на профиль пользователя
+// // Приватный роутер на профиль пользователя
+// export const config = { matcher: ['/profile', '/protected/:path*'] }\
+
+import { withAuth } from "next-auth/middleware";
+
+export const middleware = withAuth({
+    pages: {
+        signIn: '/signin'
+    },
+});
+
 export const config = { matcher: ['/profile', '/protected/:path*'] }
